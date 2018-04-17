bd2ec6618b1bdadb0eab1155c1ee6a397f18d96c
eagobert <eagobert@hotmail.com>
2018-04-17 13:03:21 -0500

    Start Home page view layout

--- VETHarbor/VETHarbor/Controllers/AccountController.cs
+++ VETHarbor/VETHarbor/Controllers/AccountController.cs
@@ -87,6 +87,7 @@ namespace VETHarbor.Controllers
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(user.UserName, model.Password, model.RememberMe, lockoutOnFailure: false);
+
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
