41b1eb95e77eb133c10f6a891480a7bf69f973c8
eagobert <eagobert@hotmail.com>
2018-04-23 06:41:07 -0500

    Update search functionality

---
+++ VETHarbor/VETHarbor/Controllers/SearchController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VETHarbor.Data;
+
+namespace VETHarbor.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SearchController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(string searchString)
+        {
+            var programs = from p in _context.Programs
+                     select p;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                programs = programs.Where(s => );
+            }
+
+            return View(await programs.ToListAsync());
+        }
+    }
+}
